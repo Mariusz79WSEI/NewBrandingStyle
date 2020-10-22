ba58e695bea3f4940c2804b656680d850a84584c
Mariusz Kalarus <mariusz.kalarus>
2020-10-22 19:28:37 +0200

    Implementacja formularza

---
+++ NewBrandingStyle.Web/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+   public class CompanyController : Controller
+   {
+      [HttpGet]
+      public IActionResult Index()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      public IActionResult Index(CompanyModel company)
+      {
+         var viewModel = new CompanyAddedViewModel
+         {
+            NumberOfCharsInName = company.Name.Length,
+            NumberOfCharsInDescription = company.Name.Length,
+            IsHidden = !company.IsVisible
+         };
+         return View("CompanyAdded", viewModel);
+      }
+   }
+}
