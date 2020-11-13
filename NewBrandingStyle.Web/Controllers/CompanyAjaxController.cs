95c8f49cd7c7ae506b88eee71182a0759ce84682
Mariusz Kalarus <mariusz.kalarus>
2020-11-13 16:32:21 +0100

    Ajax

---
+++ NewBrandingStyle.Web/Controllers/CompanyAjaxController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class CompanyAjaxController : ControllerBase
+   {
+      public CompanyModel Post()
+      {
+         var item = new CompanyModel();
+         item.Name = "Kowalski";
+         return item;
+      }
+   }
+}
---
+++ NewBrandingStyle.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,28 @@
+﻿(function () {
+   const alertElement = document.getElementById("success-alert");
+   const formElement = document.forms[0];
+
+   const addNewItem = async () => {
+      const formData = new FormData(document.getElementById('form'))
+
+      formData.forEach((key, value) => {
+         data[value] = value[key];
+      });
+
+      const requestData = {
+         method: 'POST',
+         body: JSON.stringify(data)
+      };
+
+      const responseFetch = await fetch('/api/CompanyAjax', requestData);
+
+      const response = await responseFetch.json();
+      if (response.success) {
+         alertElement.style.display = 'block';
+      }
+   };
+   window.addEventListener("load", () => {
+      formElement.addEventListener("submit", event => {
+         event.preventDefault(); addNewItem().then(() => console.log("added successfully"));
+   });
+})();
